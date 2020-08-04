fd8ac39504dd4af756e3327caaeb0ca569da1d89
Thomas Witzel <twitzel@mgh.harvard.edu>
2020-08-04 06:52:03 +0000

    Some basic cleanups, to simplify this design

--- HDL/projects/base_system_irq/block_design_Z20.tcl
+++ HDL/projects/base_system_irq/block_design_Z20.tcl
@@ -1,27 +1,12 @@
-# Create clk_wiz
-cell xilinx.com:ip:clk_wiz:6.0 pll_0 {
-  PRIMITIVE PLL
-  PRIM_IN_FREQ.VALUE_SRC USER
-  PRIM_IN_FREQ 125.0
-  PRIM_SOURCE Differential_clock_capable_pin
-  CLKOUT1_USED true
-  CLKOUT1_REQUESTED_OUT_FREQ 125.0
-  USE_RESET false
-} {
-  clk_in1_p adc_clk_p_i
-  clk_in1_n adc_clk_n_i
-}
-
 # Create processing_system7
 cell xilinx.com:ip:processing_system7:5.5 ps_0 {
-  PCW_IMPORT_BOARD_PRESET cfg/red_pitaya.xml
-  PCW_USE_S_AXI_HP0 1
+  PCW_IMPORT_BOARD_PRESET cfg/stemlab_sdr.xml
   PCW_USE_FABRIC_INTERRUPT 1 
   PCW_IRQ_F2P_INTR 1  
 } {
-  M_AXI_GP0_ACLK pll_0/clk_out1
-  S_AXI_HP0_ACLK pll_0/clk_out1
+  M_AXI_GP0_ACLK ps_0/FCLK_CLK0
 }
+
 
 # Create all required interconnections
 apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
@@ -29,6 +14,9 @@ apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
   Master Disable
   Slave Disable
 } [get_bd_cells ps_0]
+
+# Create proc_sys_reset
+cell xilinx.com:ip:proc_sys_reset:5.0 rst_0
 
 
 # Create axi_cfg_register
